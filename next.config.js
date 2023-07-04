7252095a88cde7ea47afb844de1e74770cb55203
Aarav Chauhan <aaravchauhan2211@gmail.com>
2023-07-05 00:29:37 +0530

    taking care of eslintrc warnings 3

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
+module.exports = {
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+}
+
 const nextConfig = {
     experimental: {
         appDir : true,
         serverComponentsExternalPackages: ["mongoose"],
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
     },
     images : {
         domains : ['occ-0-3933-116.1.nflxso.net'],
